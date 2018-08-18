66a984aaf4c68164e4de3de0f484eb8838d8da2a
Fake-Name <something@fake-url.com>
2018-08-17 23:12:45 -0700

    How the hell did this get deleted?
    This should fix https://github.com/fake-name/xA-Scraper/issues/184

---
+++ db_migrate.py
@@ -0,0 +1,25 @@
+#!flask/bin/python
+
+# import logging
+# logging.basicConfig()
+# logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
+from rewrite import app, db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from rewrite import database
+
+Migrate(app, db, compare_type=True)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+
+
+if __name__ == '__main__':
+	print("Running migrator!")
+	manager.run()
+
